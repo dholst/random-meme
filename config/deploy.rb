3decd39eacc79d25a92b851e0258267a754bd8bb
Darrin Holst <darrinholst@gmail.com>
2012-08-20 22:13:16 -0500

    show we're from vlad

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,10 +2,49 @@ require "bundler/vlad"
 
 set :application , "random-meme"
 set :repository  , "/etc/code/random-meme"
+set :revision    , "origin/vlad"
 set :deploy_to   , "/var/www/randommeme"
-set :domain      , "randommeme@192.168.51.50"
-set :web_command , "/etc/init.d/randommeme"
+set :perm_owner  , "randommeme"
+set :perm_group  , "randommeme"
 
-task "vlad:deploy" => %w[
-  vlad:update vlad:bundle:install vlad:start_app vlad:cleanup
-]
+def rake(command)
+  run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec rake #{command}"
+end
+
+def invoke(task)
+  Rake::Task[task].invoke
+end
+
+task :staging do
+  set :rails_env , "staging"
+  set :domain    , "randommeme@192.168.51.50"
+end
+
+namespace :vlad do
+  desc "Precompile assets"
+  remote_task :assets_precompile, :roles => :app do
+    rake "assets:precompile"
+  end
+ 
+  desc 'Restart Unicorn'
+  remote_task :start_app, :roles => :app do
+    run "/etc/init.d/randommeme restart"
+  end
+
+  desc "Deploy"
+  task :deploy do
+    puts "Updating code"
+    invoke "vlad:update"
+    puts "Installing gems"
+    invoke "vlad:bundle:install"
+    # puts "Migrating database"
+    # invoke "vlad:migrate"
+    puts "Precompiling assets"
+    invoke "vlad:assets_precompile"
+    puts "Restarting app"
+    invoke "vlad:start_app"
+    puts "Cleaning up"
+    invoke "vlad:cleanup"
+  end
+end
+
