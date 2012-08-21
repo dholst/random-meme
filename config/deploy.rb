357b6d8399c071a8362c738c19f2f604ac6f5a0b
Darrin Holst <darrinholst@gmail.com>
2012-08-21 07:50:26 -0500

    switch to the more traditional way to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,137 +2,29 @@ require "bundler/capistrano"
 
 set :application                , "random-meme"
 set :scm                        , :git
-set :repository                 , "/etc/code/random-meme"
-set :branch                     , "origin/capistrano"
+set :repository                 , "https://github.com/dholst/random-meme.git"
+set :branch                     , "capistrano"
 set :deploy_to                  , "/var/www/randommeme"
-set :normalize_asset_timestamps , false
 set :user                       , "randommeme"
 set :use_sudo                   , false
+set :deploy_via                 , :remote_cache
 
 task :staging do
   set :rails_env, "staging"
-  server "192.168.51.50", :app, :web
-end
-
-set(:latest_release)    {fetch(:current_path)}
-set(:release_path)      {fetch(:current_path)}
-set(:current_release)   {fetch(:current_path)}
-set(:current_revision)  {capture("cd #{current_path}; git rev-parse --short HEAD").strip}
-set(:latest_revision)   {capture("cd #{current_path}; git rev-parse --short HEAD").strip}
-set(:previous_revision) {capture("cd #{current_path}; git rev-parse --short HEAD@{1}").strip}
-
-desc "tail log files"
-task :tail, :roles => :app do
-  run "tail -f /var/log/www/randommeme*.log" do |channel, stream, data|
-    puts "#{channel[:host]}: #{data}"
-    break if stream == :err
-  end
+  server "192.168.51.50", :app, :web, :db, :primary => true
 end
 
 namespace :deploy do
-  namespace :assets do
-    task :precompile, :roles => :web, :except => { :no_release => true } do
-      run "cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile:primary"
-    end
-  end
-
-  desc "deploy the latest"
-  task :default do
-    update
-    restart
-  end
-
-  task :cold do
-    update
-    migrate
-  end
-
-  task :update do
-    transaction do
-      update_code
-    end
-  end
-
-  task :update_code, :except => { :no_release => true } do
-    dirs = [deploy_to, shared_path]
-    dirs += shared_children.map { |d| File.join(shared_path, d) }
-
-    cmd = []
-    cmd << "if [ ! -d #{current_path} ]; then"
-    cmd << " git clone #{repository} #{current_path};"
-    cmd << " mkdir -p #{dirs.join(" ")};"
-    cmd << " chmod g+w #{dirs.join(" ")};"
-    cmd << "fi;"
-
-    cmd << "cd #{current_path};"
-    cmd << "git fetch origin;"
-    cmd << "git reset --hard #{branch};"
-
-    run cmd.join(" ")
-    finalize_update
-  end
-
-  task :migrations do
-    transaction do
-      update_code
-    end
-    migrate
-    restart
-  end
-
-  task :finalize_update, :except => { :no_release => true } do
-    run "chmod -R g+w #{latest_release}" if fetch(:group_writable, true)
-
-    run <<-CMD
-      rm -rf #{latest_release}/log #{latest_release}/public/system #{latest_release}/tmp/pids &&
-      mkdir -p #{latest_release}/tmp &&
-      ln -s #{shared_path}/log #{latest_release}/log &&
-      ln -s #{shared_path}/system #{latest_release}/public/system &&
-      ln -s #{shared_path}/pids #{latest_release}/tmp/pids
-    CMD
-
-    if fetch(:normalize_asset_timestamps, true)
-      stamp = Time.now.utc.strftime("%Y%m%d%H%M.%S")
-      asset_paths = fetch(:public_children, %w(images stylesheets javascripts)).map { |p| "#{latest_release}/public/#{p}" }.join(" ")
-      run "find #{asset_paths} -exec touch -t #{stamp} {} ';'; true", :env => { "TZ" => "UTC" }
-    end
-  end
-
-  desc "restart unicorn"
-  task :restart, :except => { :no_release => true } do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
     run "/etc/init.d/randommeme restart"
   end
 
-  desc "start unicorn"
-  task :start, :except => { :no_release => true } do
-    run "/etc/init.d/randommeme start"
-  end
-
-  desc "stop unicorn"
-  task :stop, :except => { :no_release => true } do
-    run "/etc/init.d/randommeme stop"
-  end
-
-  namespace :rollback do
-    desc "Moves the repo back to the previous version of HEAD"
-    task :repo, :except => { :no_release => true } do
-      set :branch, "HEAD@{1}"
-      deploy.default
-    end
-
-    desc "Rewrite reflog so HEAD@{1} will continue to point to at the next previous release."
-    task :cleanup, :except => { :no_release => true } do
-      run "cd #{current_path}; git reflog delete --rewrite HEAD@{1}; git reflog delete --rewrite HEAD@{1}"
-    end
-
-    desc "Rolls back to the previously deployed version."
-    task :default do
-      rollback.repo
-      rollback.cleanup
-    end
+  task :after_update_code do
+    run "mkdir -p #{deploy_to}/#{shared_dir}/sockets"
+    run "rm -rf #{current_release}/tmp/sockets"
+    run "ln -s #{deploy_to}/#{shared_dir}/sockets #{current_release}/tmp/"
   end
 end
 
-def run_rake(cmd)
-  run "cd #{current_path}; #{rake} #{cmd}"
-end
